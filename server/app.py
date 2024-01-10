f3b9a3254300cbb1a9843813d1e8e70d590758f4
rasmi <rasminoel@gmail.com>
2024-01-10 14:22:23 +0300

    First Attempt

--- server/app.py
+++ server/app.py
@@ -6,3 +6,19 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:name>')
+def print_string(name):
+    return f'{name}'
+
+@app.route('count/<int:num>')
+def count(num):
+    return f'{num}'
+
+@app.route('/math/<int:num1>*<int:num2>')
+def math(num1, num2):
+    return f'{num1 * num2}'
